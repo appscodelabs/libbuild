21306b4c3d8a9e695fe1b0bb8276cb6e8018a909
tamal <tamal@appscode.com>
2018-01-28 04:59:02 -0800

    Fix upx command

--- libbuild.py
+++ libbuild.py
@@ -211,7 +211,7 @@ def go_build(name, goos, goarch, main, compress=False, upx=False):
     die(call(cmd, cwd=REPO_ROOT))
 
     if upx and (goarch in ['amd64', '386']):
-        cmd = "upx --brute {bindir}/{name}-{goos}-{goarch}{ext}".format(
+        cmd = "upx --brute {name}-{goos}-{goarch}{ext}".format(
                 name=name,
                 goos=goos,
                 goarch=goarch,
